3fa951c1661fe024161913ea843de79886b7d623
Mitch <hello@liquidvisual.com>
2015-06-11 15:11:22 +1000

    Code tidy up - removed some extraneous JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
 /*
-    MAIN SCRIPTS - Last updated: 00-00-00
+    MAIN SCRIPTS
 */
 //-----------------------------------------------------------------
 // Variables
 //-----------------------------------------------------------------
 
-var TOUCH_ENABLED = $(".touch").length;
+// var TOUCH_ENABLED = $(".touch").length;
 
 //-----------------------------------------------------------------
 // Document Ready
@@ -13,13 +13,12 @@ var TOUCH_ENABLED = $(".touch").length;
 
 $(document).ready(function() {
     NProgress.start(); // Start preloader bar
+    startMasonry();
 
     $(document).foundation({
         tab: {
           callback : function (tab) {
-            var grid = document.querySelector('.result-tiles');
-              var msnry = new Masonry( grid, {
-              });
+            startMasonry();
           }
         }
       });
@@ -27,50 +26,25 @@ $(document).ready(function() {
 
 $(window).load(function() {
     NProgress.done();
-
-    var grid = document.querySelector('.result-tiles');
-      var msnry = new Masonry( grid, {
-      });
+    startMasonry();
 });
 
 // window.onload = function(){
 //     // NProgress.done();
 // }
 
-$(document).ready(function() {
-    var grid = document.querySelector('.result-tiles');
-      var msnry = new Masonry( grid, {
-      });
-});
-
 //-----------------------------------------------------------------
-// Offcanvas Menu
+// Start Masonry
 //-----------------------------------------------------------------
 
-// $("#off-canvas-menu").mmenu({ "offCanvas": { "position": "right" }});
+function startMasonry() {
+  var grid = document.querySelector('.result-tiles');
+    var msnry = new Masonry( grid, {
+    });
+}
 
-//-----------------------------------------------------------------
-// Kickstart Foundation / Touch Conditionals
-//-----------------------------------------------------------------
+startMasonry();
 
-// var touchEvent = TOUCH_ENABLED ? "touchstart" : "click";
-
-//Trigger hamburger by touch on mobile - this eliminates glitch with FastClick.js
-// $('.hamburger').bind('click', function(e) {
-//     e.preventDefault();
-//     $('#off-canvas-menu').trigger('open.mm');
-// });
-
-// if (TOUCH_ENABLED) {
-    // Make Accordion jump to the top of the heading on mobile
-    // http://foundation.zurb.com/forum/posts/1316-accordion-jump-to-top-when-active
-    /*$(document).foundation('accordion', {
-        callback: function (el){
-            var containerPos = $(el).parent().offset().top;
-            $('html, body').animate({ scrollTop: containerPos }, 300);
-        }
-    });*/
-// }
 //-----------------------------------------------------------------
 // <= IE8 Caution Message
 //-----------------------------------------------------------------
