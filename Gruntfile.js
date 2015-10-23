42c37c8188e2fdcc3b10ce00214841f4aa0bf4f6
Mitch <hello@liquidvisual.com>
2015-10-23 16:15:53 +1100

    Github update

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,8 @@ module.exports = function (grunt) {
       dist: 'dist',
       assets: 'dist/assets',
       port: '9292',
-      git: 'git@github.com:liquidvisual/wlite-0615.git',
+      // git: 'git@github.com:liquidvisual/wlite-0615.git',
+      git: 'https://github.com/liquidvisual/wlite-0615.git',
       // IMPORTANT: Set a baseurl on line 364
     },
     //-----------------------------------------------------
